e1896d120652e6deee4d172eb289f2ee46eedddc
Bertrand Florat <bertrand@florat.net>
2018-03-18 15:47:52 +0100

    Ajout code initial

---
+++ ex1-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
@@ -0,0 +1,18 @@
+package com.acme.mailreader.domain;
+
+import java.time.Instant;
+
+import org.junit.Test;
+
+import com.acme.mailreader.utils.DateIncorrecteException;
+
+public class MailTest {
+	
+
+	@Test(expected=DateIncorrecteException.class)
+	public final void erreurSiDateAvant1979() throws DateIncorrecteException {
+		new Mail.Builder("sujet").date(Instant.parse("1968-12-03T10:15:30.00Z")).build();
+				
+	}
+
+}
