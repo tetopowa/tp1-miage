5da5a830000eed8416fdf8409866780d9776c968
tetopowa <theo.lacroix@live.fr>
2018-03-22 11:34:14 +0100

    modification mailtest

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
@@ -1,18 +1,33 @@
 package com.acme.mailreader.domain;
 
-import org.junit.Ignore;
+import java.time.Instant;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import com.acme.mailreader.utils.DateIncorrecteException;
 
 public class MailTest {
-	
 
-	@Ignore
 	@Test(expected=DateIncorrecteException.class)
-	public final void erreurSiDateAvant1979() throws DateIncorrecteException {
-		//TODO
-				
+	public final void erreurSiDateAvant1970() throws DateIncorrecteException {
+		Mail mail = new Mail();
+		mail.setDate(Instant.parse("1969-01-01T00:00:00.00Z"));
+	}
+	
+	@Test(expected=DateIncorrecteException.class)
+	public final void erreurSiDateApres2100() throws DateIncorrecteException {
+		Mail mail = new Mail();
+		mail.setDate(Instant.parse("2222-01-01T00:00:00.00Z"));
 	}
 
+	@Test
+	public final void getDateOk() throws DateIncorrecteException{
+		Mail mail = new Mail();
+		mail.setDate(Instant.parse("2000-01-01T00:00:00.00Z"));
+		assertThat(mail.getDate(), is(Instant.parse("2000-01-01T00:00:00.00Z")));
+	}
 }
